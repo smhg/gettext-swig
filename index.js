d0c43416205a4bc429cb4b59992cbaff8b93c0fb
smhg <sam@strictlyphp.com>
2014-11-26 12:21:40 +0100

    regex parser

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var newline = /\r?\n|\r/g,
+  escapeRegExp = function (string) {
+      // source: https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions
+      return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+    },
+  trimQuotes = function (str) {
+      return str.replace(/^['"]/g, '').replace(/['"]$/g, '');
+    },
+  paramPattern = new RegExp('([\'"][^\'"]*[\'"]|[^\'" ]+)', 'g');
+
 /**
  * Constructor
  * @param Object keywordSpec An object with keywords as keys and parameter indexes as values
@@ -14,19 +24,43 @@ function Parser (keywordSpec) {
   }
 
   this.keywordSpec = keywordSpec;
+  this.expressionPattern = new RegExp([
+    '{%-? +',
+    '(' + Object.keys(keywordSpec).map(escapeRegExp).join('|') + ')',
+    ' +',
+    '([\\s\\S]*?)',
+    ' +-?%}'
+    ].join(''), 'g');
 }
 
 /**
- * Given a Handlebars template string returns the list of i18n strings.
+ * Given a Swig template string returns the list of i18n strings.
  *
  * @param String template The content of a HBS template.
  * @return Object The list of translatable strings, the line(s) on which each appears and an optional plural form.
  */
 Parser.prototype.parse = function (template) {
-  var keywordSpec = this.keywordSpec,
-    keywords = Object.keys(keywordSpec);
+  var result = {},
+    match,
+    keyword,
+    params,
+    msgid;
 
-  return {};
+  while ((match = this.expressionPattern.exec(template)) !== null) {
+    keyword = match[1];
+    params = match[2].match(paramPattern).map(trimQuotes);
+
+    msgid = params[this.keywordSpec[keyword][0]];
+
+    result[msgid] = result[msgid] || {line: []};
+    result[msgid].line.push(template.substr(0, match.index).split(newline).length);
+
+    if (this.keywordSpec[keyword].length > 1) {
+      result[msgid].plural = result[msgid].plural || params[this.keywordSpec[keyword][1]];
+    }
+  }
+
+  return result;
 };
 
 module.exports = Parser;
