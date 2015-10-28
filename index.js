5298238092cda1d4d8fd8ed3d8237a4037707eff
Kukushkin Alexey <domio.art@gmail.com>
2015-10-28 18:12:34 +0300

    fix  {{ expression }}

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var newline = /\r?\n|\r/g,
+var newline      = /\r?\n|\r/g,
   escapeRegExp = function (str) {
     // source: https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions
-    return str.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+    return str.replace(/([.*+?^${}()|\[\]\/\\])/g, '\\$1');
   },
-  trim = function (str) {
+  trim         = function (str) {
     return str.replace(/^\s+|\s+$/g, '');
   },
-  trimQuotes = function (str) {
+  trimQuotes   = function (str) {
     return str.replace(/^['"]|['"]$/g, '');
   },
-  isQuote = function (chr) {
+  isQuote      = function (chr) {
     return /['"]/.test(chr);
   },
-  groupParams = function (result, part) {
+  groupParams  = function (result, part) {
     if (result.length > 0) {
-      var last = result[result.length - 1],
+      var last      = result[result.length - 1],
         firstChar = last[0],
-        lastChar = last[last.length - 1];
+        lastChar  = last[last.length - 1];
 
       if (isQuote(firstChar) && (!isQuote(lastChar) || last[last.length - 2] === '\\')) {
         // merge with previous
@@ -47,13 +47,13 @@ function Parser(keywordSpec) {
     throw 'Invalid keyword spec';
   }
 
-  this.keywordSpec = keywordSpec;
-  this.expressionPattern = new RegExp([
+  this.keywordSpec           = keywordSpec;
+  this.expressionPattern     = new RegExp([
     '(?:(.)?(' + Object.keys(keywordSpec).map(escapeRegExp).join('|') + ')',
     '\\(((?:(["\'])(.|\\t|\\n|\\r)*?\\4(?:,\\s?)*(?:[\\w\\.[\\],\\s+-/()]+)*)*)\\))'
   ].join(''), 'g');
-  this.swigExpressionPattern = /{{((?:(?!{{)(?:.|\t|\n|\r))*)}}|{%((?:(?!{%)(?:.|\t|\n|\r))*)%}/g;
-  this.keyWordExist = new RegExp(Object.keys(keywordSpec).map(escapeRegExp).join('|'))
+  this.swigExpressionPattern = /{{((?:(?!{{)(?!}})(?:.|\t|\n|\r))*)(?:}}|\|)|{%((?:(?!{%)(?:.|\t|\n|\r))*)%}/g;
+  this.keyWordExist          = new RegExp(Object.keys(keywordSpec).map(escapeRegExp).join('|'));
 }
 
 /**
@@ -69,9 +69,12 @@ Parser.prototype.parse = function (template) {
     params,
     expMatch,
     msgid;
-
+  if (template.indexOf('billing_balance_offert') != -1) {
+    console.log(template);
+  }
   while ((expMatch = this.swigExpressionPattern.exec(template)) !== null) {
     var expResult = expMatch[1] || expMatch[2];
+
     if (!this.keyWordExist.test(expResult)) {
       continue;
     }
@@ -79,9 +82,9 @@ Parser.prototype.parse = function (template) {
       if (match[1] !== ' ' && match[1] !== '.' && match[1] !== undefined) {
         continue;
       }
-      keyword = match[2];
-      params = match[3].split(',').reduce(groupParams, []).map(trim).map(trimQuotes);
-      msgid = params[this.keywordSpec[keyword][0]];
+      keyword       = match[2];
+      params        = match[3].split(',').reduce(groupParams, []).map(trim).map(trimQuotes);
+      msgid         = params[this.keywordSpec[keyword][0]];
       result[msgid] = result[msgid] || {line: []};
       result[msgid].line.push(template.substr(0, match.index).split(newline).length);
 
